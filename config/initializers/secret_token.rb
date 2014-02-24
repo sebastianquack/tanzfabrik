e856e7b7f681e1d50dfe7c56d7dcd2975993efaf
Sebastian Quack <sebastianquack@gmail.com>
2014-02-24 11:36:12 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tanzfabrik::Application.config.secret_key_base = '20261f9e87c3d30fed733f3f391668d0151d46d5e69e4b7454e370429f312d4a735b089b562127d5761326816047ca37359a1541bb5ffe33b939b4bfae8ace4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tanzfabrik::Application.config.session_store :cookie_store, key: '_tanzfabrik_session'
