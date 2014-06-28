862bf40fe03d70f0170f245d69c9b7820ef10f5e
retani <holgheiss@gmail.com>
2014-06-28 22:35:28 +0200

    make content disappear behind submenu style performance_projecte table as UL

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,36 @@ $(document).ready(function() {
     $(this).parents('.open-close').removeClass("opened")
   });
 
+  initMenuContentHide()
+
 });
+
+var initMenuContentHide = function () {
+  last_menu3Height = null
+  $("#top-menu ul ul li").on("mouseenter mouseleave", function () { 
+    menu3 = $("#top-menu ul ul ul:visible")
+    menu3Height = menu3.outerHeight()
+    if (last_menu3Height != menu3Height) {
+      last_menu3Height = menu3Height
+      if (menu3Height == null) {
+        delta = 0
+      }
+      else {
+        menu3Offset = menu3.offset().top
+        contentOffset = $("#content-container").offset().top
+        delta = menu3Offset + menu3Height - contentOffset
+      }
+      if (delta > 0) {
+        $("#content").css("margin-top", -delta + "px")
+        $("#content-container").css("top", delta + "px")
+      }
+      else {
+        $("#content").css("margin-top", "0px")
+        $("#content-container").css("top", "0px")
+      }
+    }
+  })
+}
 
 var setMaxHeight = function(elem) {
     var oldMaxHeight = elem.css("max-height")
