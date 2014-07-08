6d4c39306d0cd0bc1e2d7c758ea24dd590bb2924
retani <holgheiss@gmail.com>
2014-07-08 20:47:10 +0200

    translate time.day. try to order EventPerson by person.name in model (not successful)

--- app/models/person_event.rb
+++ app/models/person_event.rb
@@ -1,4 +1,5 @@
 class PersonEvent < ActiveRecord::Base
   belongs_to :event
   belongs_to :person
+  # default_scope { joins(:person).order('people.name ASC') } # how to make default order right?
 end
