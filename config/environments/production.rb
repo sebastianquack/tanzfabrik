8ca6b69b875217b71516a18d4a1d45458539df5e
Sebastian Quack <sebastianquack@gmail.com>
2014-08-01 16:35:50 +0200

    bug in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Tanzfabrik::Application.configure do
     port: "587",
     authentication: "plain",
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD']
-    domain: 'heroku.com'
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
     enable_starttls_auto: true,
 
   }
