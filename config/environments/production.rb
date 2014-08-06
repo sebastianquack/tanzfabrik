ad4d344a8cdc6466c43abd9454c52a6641eefc19
retani <holgheiss@gmail.com>
2014-08-06 22:49:25 +0200

    cache: use memory_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Tanzfabrik::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.cache_store = :memory_store
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -33,7 +34,7 @@ Tanzfabrik::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
