14a675352cf4db972cb5e962d0df203f19b1a8d0
retani <holgheiss@gmail.com>
2014-10-21 14:22:32 +0200

    reset_occurrences in migration

--- db/migrate/20141015155701_create_event_detail_occurrences.rb
+++ db/migrate/20141015155701_create_event_detail_occurrences.rb
@@ -1,11 +1,11 @@
 class CreateEventDetailOccurrences < ActiveRecord::Migration
   def change
-    create_table :event_detail_occurrences do |t|
+    create_table :event_detail_occurrences, force: true do |t|
       t.integer :event_id
       t.integer :event_detail_id
       t.datetime :time
-
       t.timestamps
     end
+    EventDetail.all.each { |ed| ed.reset_occurrences } 
   end
 end
