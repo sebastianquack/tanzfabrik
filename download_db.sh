6c4ff4ef197579681e61fce37eb9b56e140ee5b2
retani <holgheiss@gmail.com>
2015-06-03 18:26:10 +0200

    adjust to new syntax, see https://devcenter.heroku.com/articles/heroku-postgres-backups

--- download_db.sh
+++ download_db.sh
@@ -1,3 +1,3 @@
-heroku pgbackups:capture --expire --app tanzfabrik
-curl -o /tmp/latest.dump `heroku pgbackups:url --app tanzfabrik`
+heroku pg:backups capture --app tanzfabrik
+curl -o /tmp/latest.dump `heroku pg:backups public-url --app tanzfabrik`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d tanzfabrik/development /tmp/latest.dump
