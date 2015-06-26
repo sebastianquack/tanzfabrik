e17eca6d859e5b728b9c73e5840f5876d00f80b7
Sebastian Quack <sebastianquack@gmail.com>
2015-06-26 17:39:14 +0200

    ckeditor integration

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,12 +1,30 @@
 #= require active_admin/base
 //= require jquery
+//= require jquery_ujs
+//= require ckeditor-jquery
+
 //= require bootstrap
-//= require bootstrap-wysihtml5
-//= require bootstrap-wysihtml5/locales/de-DE
 
 $(document).ready(function() {
   // $(".edit_link, .view_link").addClass("button")
 
+  //console.log(wysihtml5)
+
+
+  $('.wysihtml5').ckeditor({
+    language: "de",
+    width: '79%',
+    toolbar: [
+      { name: 'format', items: ['Bold', 'Italic', 'Link', 'Unlink'] },
+      { name: 'undo', items: ['Undo', 'Redo'] },
+      { name: 'copy', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'] },
+      { name: 'document', items: [ 'Source' ] }
+    ]
+    
+  });  
+
+
+/*
   $('.wysihtml5').wysihtml5({
     "toolbar": {
       "image": false, 
@@ -26,5 +44,9 @@ $(document).ready(function() {
       }
     }
   });
+*/
+  
+    
+  
 })
 
