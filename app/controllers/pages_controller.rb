22e457c2e10e27389d1bbf2139ece5a83461b7b7
Sebastian Quack <Sebastian Quack>
2020-05-22 15:02:49 +0200

    add new empty layout for relaunch and testing with url param

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
 
   # GET /pages/:id
   def show
+
+    @active_layout = request.GET["2020"] ? "application2020" : "application"
+
     # check if slug was passed in
     if params[:id]
       #use regular query because friendly throws error if not found
@@ -12,13 +15,13 @@ class PagesController < ApplicationController
         set_meta_tags :description => choose_page_description(@page)
         # try to show special template with page slug name if exists      
         if !Dir.glob(Rails.root.join("app", "views", "pages", "#{@page.slug}.*")).empty?
-          render "#{@page.slug}" and return
+          render "#{@page.slug}", :layout => @active_layout and return
         end
       else
         # if page doesn't exist in db, check if template exists
         if !Dir.glob(Rails.root.join("app", "views", "pages", "#{params[:id]}.*")).empty?
           # if yes, render that template
-          render "#{params[:id]}" and return
+          render "#{params[:id]}", :layout => @active_layout and return
         else
           # if not, redirect to root in order to clean up url
           redirect_to root_path
@@ -27,8 +30,10 @@ class PagesController < ApplicationController
     else
       # no params passed in, go to default page
       @page = Page.friendly.find('start')
-      render "start" and return # this works even if page start is not in db and exists only as template
+      render "start", :layout => @active_layout and return # this works even if page start is not in db and exists only as template
     end
+
+    render :layout => @active_layout
 
   end
 
