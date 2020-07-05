901586ed3f8bf2177ac3ff1251c78a8a6a504747
Sebastian Quack <Sebastian Quack>
2020-07-05 21:10:15 +0200

    fix more migrations

--- db/migrate/20200528094505_add_ancestry_to_menu_items.rb
+++ db/migrate/20200528094505_add_ancestry_to_menu_items.rb
@@ -1,6 +1,6 @@
 class AddAncestryToMenuItems < ActiveRecord::Migration[4.2]
   def change
-    add_column :menu_items, :ancestry, :string
+    add_column :menu_items, :ancestry, :string if not column_exists?(:menu_items, :ancestry)
     add_index :menu_items, :ancestry
   end
 end
