cc828810f2b7ad4796edced1ba0d61d27c470679
Sebastian Quack <Sebastian Quack>
2020-09-07 21:31:57 +0200

    show section landing page for pages not in main menu hierarchy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,10 +8,25 @@ class PagesController < ApplicationController
     @menu_tree = {}
     @max_depth = 3
 
+    # this finds the appropriate menu item from the section set in page
+    def find_menu_item_for_orphan
+      @menu_item = MenuItem.find_by key: @page.section
+      if @menu_item
+        @menu_tree = @menu_item.descendants.arrange(:order => :position)
+      end
+      if @page.section 
+        section_landing_page = Page.where(slug: @page.section).first
+        if section_landing_page
+          @landing_page_menu_item = MenuItem.where(page_id: section_landing_page.id).first
+        end
+      end
+    end
+
     # check if slug was passed in
     if params[:id]
       #use regular query because friendly throws error if not found
       @page = Page.no_draft.where(:slug => params[:id]).first 
+      
       if @page
         set_meta_tags :title => @page.title
         set_meta_tags :description => choose_page_description(@page)
@@ -19,24 +34,24 @@ class PagesController < ApplicationController
         # load menu items referencing the page and use the one that is the deepest in the tree 
         menu_items = MenuItem.where(page_id: @page.id).sort {|a, b| b.depth <=> a.depth}
 
-        if menu_items[0] 
+        if menu_items[0]
           @menu_item = menu_items[0]
           # always show menu from landing pages down
-          if menu_items[0].ancestors.length == 1 # this is a landing page, show full subtree
+          # this is a landing page, show full subtree
+          if menu_items[0].ancestors.length == 1 and ["buehne", "schule", "fabrik"].include? @page.slug
             @menu_tree = menu_items[0].descendants.arrange(:order => :position) 
             @landing_page_menu_item = menu_items[0]
           elsif menu_items[0].ancestors.length > 1 # we are deeper, show from depth 1 down
             @menu_tree = menu_items[0].ancestors[1].descendants.arrange(:order => :position) 
             @landing_page_menu_item = menu_items[0].ancestors[1]
+          else
+            # this is a menu item that is not in the tree
+            find_menu_item_for_orphan
           end
 
-          
         else 
           # page is not in nav tree, use section as starting point 
-          @menu_item = MenuItem.find_by key: @page.section
-          if @menu_item
-            @menu_tree = @menu_item.descendants.arrange(:order => :position)
-          end
+          find_menu_item_for_orphan
         end
 
       else
