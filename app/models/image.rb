c6054ed78ebc72e0d430e8f5f9535999ea05ac43
Holger H <holgheiss@gmail.com>
2020-09-15 13:43:44 +0200

    add width and height to image, maybe be used to tell orientation

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,12 +4,13 @@ class Image < ActiveRecord::Base
   has_attached_file :attachment,
     :styles => { 
       :background => "1500x1500>", 
-      :large => ["800x800>"],
+      :large => ["1000x1000>"],
       :medium_detail_column => ["330>"], 
       :people => ['150x195#'],
       :people_hires => ['300x390#'],
       :medium => ["450x450>"], 
-      :thumb => ["100x100>"]
+      :thumb => ["100x100>"],
+      :logo => ["300x300>"],
       },
     :convert_options => { 
       :background => "-quality 90 -strip",
@@ -18,6 +19,7 @@ class Image < ActiveRecord::Base
       :people => "-quality 80", 
       :people_hires => "-quality 80",
       :medium  => "-quality 85",
+      :logo  => "-quality 90",
       },
     :path => "images/:id/:style.:extension"
   
@@ -44,6 +46,8 @@ class Image < ActiveRecord::Base
   belongs_to :text_item
 
   before_validation :clean_link, :fix_trix
+  before_save  :extract_dimensions 
+
 
   private def fix_trix
     self.rich_content_1_de = ModelHelpers.fix_trix self.rich_content_1_de.to_s
@@ -54,5 +58,17 @@ class Image < ActiveRecord::Base
     self.link_href_en = ModelHelpers.strip_domain_from_link self.link_href_en.to_s
     self.link_href_de = ModelHelpers.strip_domain_from_link self.link_href_de.to_s
   end  
+
+  private def extract_dimensions
+    geometry = Paperclip::Geometry.from_file(attachment.queued_for_write[:original].path)
+    #return unless geometry
+    puts "XXX"
+    puts geometry.inspect
+    puts geometry.horizontal?
+    puts geometry.width
+    puts geometry.width.to_i
+    self.width = geometry.width.to_i
+    self.height = geometry.height.to_i
+  end
 
 end
