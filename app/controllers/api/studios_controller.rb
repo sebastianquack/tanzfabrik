19f87257a311ce501a1b00f75bece88f6e842836
camfeghali <camfeg@gmail.com>
2024-01-28 18:29:29 +0100

    successfully returns available slots given a time range, currently booked time slots and a slot size

--- app/controllers/api/studios_controller.rb
+++ app/controllers/api/studios_controller.rb
@@ -1,69 +1,113 @@
 module Api  
   class StudiosController < ApplicationController
     def index
-      open_time = 
-
       now = to_iso_date_string(Date.today)
       to = to_iso_date_string(Date.today >> 2)
       booked_slots = Calendar.all.first.events.where("start_time >= ? AND end_time <= ?", now, to).order(start_time: :asc)
-      calendar = build_calendar_availabilities(14, booked_slots)
-      render json: {calendar: calendar, booked_slots: booked_slots}
+      merged_bookings_by_day = merge_bookings_by_day(booked_slots)
+      availabilities = build_calendar_availabilities(28, merged_bookings_by_day)
+      render json: availabilities
     end
 
     private
 
-    def to_iso_date_time(date)
-      Time.utc(date.year, date.month, date.day, 0, 0, 0).strftime("%Y-%m-%dT%H:%M:%S")
+    def merge_bookings_by_day(intervals)
+      return [] if intervals.empty?
+      res = []
+      booked_time = {start_time: intervals[0].start_time, end_time: intervals[0].end_time}
+      for i in 1..intervals.size - 1
+        if intervals[i].start_time <= booked_time[:end_time]
+          booked_time[:end_time] = max_date(booked_time[:end_time], intervals[i].end_time)
+        else
+          res << booked_time
+          booked_time = {start_time: intervals[i].start_time, end_time: intervals[i].end_time}
+        end
+      end
+      merge_by_day(res)
     end
 
-    def from_iso_datetime_string(datetime_string)
-      DateTime.parse(datetime_string)
+    def merge_by_day(events)
+      merged = events.reduce({}) do |result, event|
+        start_time = event[:start_time]
+        end_time = event[:end_time]
+
+        day = to_iso_date_string(start_time)
+
+        if result.key?(day)
+          result[day] << { "start_time" => start_time.to_datetime, "end_time" => end_time.to_datetime }
+        else
+          result[day] = [{ "start_time" => start_time.to_datetime, "end_time" => end_time.to_datetime }]
+        end
+        result
+      end
+      merged
+    end
+
+    def max_date date1, date2
+      date1 > date2 ? date1 : date2
+    end
+
+    def to_iso_date_time(date)
+      Time.utc(date.year, date.month, date.day, 0, 0, 0).strftime("%Y-%m-%dT%H:%M:%S")
     end
 
     def to_iso_date_string(date)
       Time.utc(date.year, date.month, date.day).strftime("%Y-%m-%d")
     end
 
-    def is_same_day(date1, date2)
-      date1.year == date2.year && date1.month == date2.month && date1.day == date2.day
+    def from_iso_datetime_string(datetime_string)
+      DateTime.parse(datetime_string)
     end
 
-    def build_calendar_availabilities(days_forward, reservations)
-      opening_hour = 4
-      closing_hour = 23
+    def berlin_time(datetime)
+      datetime.utc.in_time_zone("Europe/Amsterdam")
+    end
+
+    def build_calendar_availabilities(days_forward, reservations_by_day)
+      opening_hour = 7
+      closing_hour = 17
       slot_time = 2
       
-      cur_day = Date.today
-
       calendar = {}
-      
-      res_pointer = 0;
-      next_reservation = reservations[res_pointer]
-      
-      days_forward.times do |i|
-        date = cur_day + i
-        calendar[date] = []
-        cur_time = DateTime.new(date.year, date.month, date.day, opening_hour, 0, 0)
-        closes_at = DateTime.new(date.year, date.month, date.day, closing_hour, 0, 0)
-        
-        while cur_time + Rational(slot_time, 24) <= closes_at
-          next_reservation_start_time = from_iso_datetime_string(next_reservation.start_time.to_s)
-          next_reservation_end_time = from_iso_datetime_string(next_reservation.end_time.to_s)
-          Rails.logger.warn("#{cur_time}")
-          Rails.logger.warn("#{next_reservation_start_time} - #{next_reservation_end_time}")
 
-          if cur_time + Rational(slot_time, 24) <= next_reservation_start_time
-            calendar[date] << cur_time
-            cur_time = cur_time + Rational(slot_time, 24)
+      for i in 0..days_forward do
+        cur_day = Date.today + i
+        cur_day_key = to_iso_date_string(cur_day)
+        calendar[cur_day_key] = []
+        reservations = reservations_by_day[cur_day_key]
+
+        cur_time = DateTime.new(cur_day.year, cur_day.month, cur_day.day, opening_hour, 0, 0)
+        closes_at = DateTime.new(cur_day.year, cur_day.month, cur_day.day, closing_hour, 0, 0)
+
+        if reservations.nil?
+          while cur_time + Rational(slot_time, 24) <= closes_at
+            calendar[cur_day_key] << berlin_time(cur_time)
+            cur_time += Rational(slot_time, 24)
+          end
+          next
+        end
+
+        r_p = 0
+        next_reservation = reservations[r_p]
+
+        while cur_time + Rational(slot_time, 24) <= closes_at
+          if next_reservation.nil?
+            while cur_time + Rational(slot_time, 24) <= closes_at
+              calendar[cur_day_key] << berlin_time(cur_time)
+              cur_time += Rational(slot_time, 24)
+            end
+            break
+          end
+          if cur_time + Rational(slot_time, 24) < next_reservation["start_time"]
+            calendar[cur_day_key] << berlin_time(cur_time)
+            cur_time += Rational(slot_time, 24)
           else
-            cur_time = next_reservation_end_time
-            res_pointer += 1
-            next_reservation = reservations[res_pointer]
+            cur_time = next_reservation["end_time"]
+            r_p += 1
+            next_reservation = reservations[r_p]
           end
         end
-        Rails.logger.warn("__________________________________________")
       end
-
       return calendar
     end
   end
