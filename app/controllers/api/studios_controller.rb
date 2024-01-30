d983f259d9929dadbae545c9bc3fbd9a3ed90da1
camfeghali <camfeg@gmail.com>
2024-01-30 20:37:15 +0100

    refactored code organization, moved model access to the controller

--- app/controllers/api/studios_controller.rb
+++ app/controllers/api/studios_controller.rb
@@ -2,8 +2,12 @@ module Api
   class StudiosController < ApplicationController
     def index
       now = DateHelper.to_iso_date_string(Date.today)
-      to = DateHelper.to_iso_date_string(Date.today >> 12)
-      availabilities = Calendar.find_by(studio_id: params[:id]).get_availabilities(now, to)            
+  
+      booking_type = BookingType.find_by(name: params[:booking_type])
+      calendar = Calendar.find_by(studio_id: params[:id])
+      availability_config = CalendarBookingType.find_by(calendar: calendar, booking_type: booking_type).settings
+      availabilities = calendar.get_availabilities(now, availability_config)
+
       render json: availabilities
     end
   end
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -5,60 +5,24 @@ class Calendar < ActiveRecord::Base
   has_many :availabilities, class_name: :CalendarBookingType
   has_many :booking_types, through: :availabilities
 
-  def get_availabilities(from, to)
+  def get_availabilities(from, availability_config)
+    to = DateHelper.to_iso_date_string(Date.today >> availability_config["bookable_months_in_advance"])
     upcoming_events = events.where("start_time >= ? AND end_time <= ?", from, to).order(start_time: :asc)
-    merged_bookings_by_day = merge_bookings_by_day(upcoming_events)
-    return build_calendar_availabilities(364, merged_bookings_by_day)
+    merged_bookings_by_day = merge_bookings_by_day(upcoming_events)    
+    return build_calendar_availabilities(availability_config, merged_bookings_by_day)
   end
 
   private
 
-  def merge_bookings_by_day(intervals)
-    return [] if intervals.empty?
-    res = []
-    booked_time = {start_time: intervals[0].start_time, end_time: intervals[0].end_time}
-    for i in 1..intervals.size - 1
-      if intervals[i].start_time <= booked_time[:end_time]
-        booked_time[:end_time] = DateHelper.max_date(booked_time[:end_time], intervals[i].end_time)
-      else
-        res << booked_time
-        booked_time = {start_time: intervals[i].start_time, end_time: intervals[i].end_time}
-      end
-    end
-    group_by_day(res)
-  end
-
-  def group_by_day(events)
-    merged = events.reduce({}) do |result, event|
-      start_time = event[:start_time]
-      end_time = event[:end_time]
-
-      day = DateHelper.to_iso_date_string(start_time)
-
-      if result.key?(day)
-        result[day] << { "start_time" => start_time.to_datetime, "end_time" => end_time.to_datetime }
-      else
-        result[day] = [{ "start_time" => start_time.to_datetime, "end_time" => end_time.to_datetime }]
-      end
-      result
-    end
-    merged
-  end
-
-  def availability_by_booking_type(booking_type_name)
-    booking_type = BookingType.where(name: booking_type_name).first
-    self.availabilities.where(booking_type_id: booking_type.id).first.settings
-  end
-
-  def build_calendar_availabilities(days_forward, reservations_by_day)
+  def build_calendar_availabilities(availability_config, reservations_by_day)
     slot_time = 2
-    
+    days_forward = availability_config["bookable_months_in_advance"]*30
     calendar = {}
 
     for i in 0..days_forward do
       cur_day = Date.today + i
       day_of_the_week = cur_day.strftime('%A').downcase
-      availability_of_the_day = availability_by_booking_type("Rehearsal 2hrs")["schedule"][day_of_the_week]
+      availability_of_the_day = availability_config["schedule"][day_of_the_week]
 
       cur_day_key = DateHelper.to_iso_date_string(cur_day)
       calendar[cur_day_key] = []
@@ -107,4 +71,37 @@ class Calendar < ActiveRecord::Base
     end
     return calendar
   end
+
+  def merge_bookings_by_day(intervals)
+    return [] if intervals.empty?
+    res = []
+    booked_time = {start_time: intervals[0].start_time, end_time: intervals[0].end_time}
+    for i in 1..intervals.size - 1
+      if intervals[i].start_time <= booked_time[:end_time]
+        booked_time[:end_time] = DateHelper.max_date(booked_time[:end_time], intervals[i].end_time)
+      else
+        res << booked_time
+        booked_time = {start_time: intervals[i].start_time, end_time: intervals[i].end_time}
+      end
+    end
+    group_by_day(res)
+  end
+
+  def group_by_day(events)
+    merged = events.reduce({}) do |result, event|
+      start_time = event[:start_time]
+      end_time = event[:end_time]
+
+      day = DateHelper.to_iso_date_string(start_time)
+
+      if result.key?(day)
+        result[day] << { "start_time" => start_time.to_datetime, "end_time" => end_time.to_datetime }
+      else
+        result[day] = [{ "start_time" => start_time.to_datetime, "end_time" => end_time.to_datetime }]
+      end
+      result
+    end
+    merged
+  end
+
 end
